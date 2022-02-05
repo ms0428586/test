517b194008013d17656931818d32dd111986c85c
Linminxuan923 <40641136@gm.nfu.edu.tw>
2022-02-06 01:07:34 +0800

    0226

--- JAVA/zootreekey01.sql
+++ JAVA/zootreekey01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost:3306
--- 產生時間： 2022-02-04 20:47:23
+-- 產生時間： 2022-02-06 01:06:36
 -- 伺服器版本： 5.7.24
 -- PHP 版本： 7.4.16
 
@@ -66,7 +66,7 @@ CREATE TABLE `memberinfo` (
 
 INSERT INTO `memberinfo` (`id`, `account`, `name`, `password`, `email`, `phone`) VALUES
 (1, 'qazqaz', '美女', 'qazwsx', 'qq70010pp@yahoo.com.tw', 98778778),
-(2, 'zootreekey', '老六', 'qazwsx', 'qqwwwwwpp@yahoo.com.tw', 987000000),
+(2, 'zootreekey', '老七', 'qazwsx', 'qq170010pp@yahoo.com.tw', 979178176),
 (3, 'qweasd', '帥哥', 'qweasd', '878@gmail.com', 987878887),
 (4, 'zxczxc', '賭神', 'zxczxc', '123@gmail.com', 979178176),
 (5, 'asdasd', '大帥哥', 'asdasd', '321@gmail.com', 979178176);
@@ -99,20 +99,45 @@ INSERT INTO `restaurant` (`restaurant_id`, `restaurant_category`, `restaurant_na
 (1, '速食店', '麥當勞-台中公益二店', '台中市南屯區公益路二段128號', '$$', '+886423285051', '00:00-24:00', '台中市', '/img/RestaurantLogoImg/McDonald.jpg', 'https://www.mcdonalds.com/tw/zh-tw.html', 'https://goo.gl/maps/dv9kGjYfHagRuWie9'),
 (2, '速食店', '肯德基KFC-台中公益餐廳', '台中市西區公益路163號', '$$', '+886423027066', '11:00-23:00', '', '/img/RestaurantLogoImg/KFC.png', 'https://www.facebook.com/kfctaiwan/', 'https://goo.gl/maps/B7uP7PJ7sqLAUs8K9'),
 (3, '速食店', '漢堡王Burger King台中公益店', '台中市西區公益路152-5號', '$$', '+886423200922', '10:00-22:00', '台中市', '/img/RestaurantLogoImg/BurgerKing.jpg', 'https://www.burgerking.com.tw/branchweb', 'https://goo.gl/maps/7i3enNzZ85FkH14EA'),
-(4, '韓式料理', '咕咕家韓式炸雞Pizza吃到飽 公益店', '臺中市南屯區公益路二段95號1樓', '$$$', '+886423208284', '11:00-22:00', '台中市', NULL, 'https://www.facebook.com/ChickenPlusGongYi/', 'https://goo.gl/maps/Js4Dx2gDM8uRsao87'),
-(5, '火鍋', '和牛涮 日式鍋物放題 台北羅斯福店', '台北市大安區羅斯福路二段27號', '$$$$', '+886223910269', '11:30-23:30', '台北市', NULL, 'https://www.wagyushabu.com.tw/', 'https://goo.gl/maps/CsFfh3sz7g4nBSg68');
+(6, '港式', '廚窗港點 士林官邸店', '臺北市士林區福林路190跟188號', '均消$660', '0228806565', '11:30-14:30\r\n17:30-21:00', '台北市', '/img/RestaurantLogoImg/台北-廚窗港點.jpg', 'https://reurl.cc/44dDyj', 'https://reurl.cc/OpraX3'),
+(7, '台式', '款待 手路菜', '臺北市中山區中山北路一段53巷32號', '均消$1100', '0225676097', '11:30-14:00\r\n17:30-21:30', '台北市', '/img/RestaurantLogoImg/台北-款待台菜.jpeg', 'https://reurl.cc/7edAry', 'https://reurl.cc/2DjArE'),
+(8, '日式\r\n燒肉\r\n火鍋', '金洹苑 KIN KAN EN', '臺北市大安區延吉街131巷31號', '均消$1100', '0287720109', '11:30-14:30\r\n17:30-22:00', '台北市', '/img/RestaurantLogoImg/台北-金洹苑.jpeg', 'https://reurl.cc/jky48m', 'https://reurl.cc/3j4Aa9'),
+(9, '台式\r\n麵包\r\n糕點', '和泰興', '新北市板橋區陽明街3號', '均消$60', '0222582562', '08:00-22:30', '新北市', '/img/RestaurantLogoImg/台北-和泰興.jpeg', 'https://reurl.cc/l9yb5d', 'https://reurl.cc/5GdL2M'),
+(10, '泰式', '水上米 泰式料理餐廳', '新北市新莊區幸福路821號', '均消$250', '0287899789', '11:00-14:00\r\n16:30-21:00', '新北市', '/img/RestaurantLogoImg/台北-水上米.jpeg', 'https://reurl.cc/3j4A59', 'https://reurl.cc/WkNYXe'),
+(11, '韓式\r\n燒肉', '咚豬咚豬。韓國烤肉吃到飽（台中公益大墩店）', '臺中市南屯區大墩路696號', '均消$399', '0423272227', '11:30-16:00\r\n17:00-22:30', '台中市', '/img/RestaurantLogoImg/台中-咚豬咚豬.jpeg', 'https://reurl.cc/jky451', 'https://reurl.cc/ZrVd7p'),
+(12, '火鍋', '宮秸火鍋', '臺中市南屯區益豐路四段500號', '均消$698', '0423829589', '11:30-01:00', '台中市', '/img/RestaurantLogoImg/台中-宮秸火鍋.png', 'https://reurl.cc/Y9qAWx', 'https://reurl.cc/Ep69n1'),
+(13, '義式\r\n美式', '眺高 STAY WITH MOUNTAIN', '臺中市北屯區部子路700巷11弄36號', '均消$500', '0424390886', '一～五 16:00-00:00\r\n六日 11:30-00:00', '台中市', '/img/RestaurantLogoImg/台中-眺高.jpeg', 'https://reurl.cc/9O3K4v', 'https://reurl.cc/xOvr7N'),
+(14, '台式', '千田屋（草悟店）', '臺中市西區公正路130之1號', '均消$150', '0423028911', '11:30-14:30\r\n17:00-20:30\r\n週二公休', '台中市', '/img/RestaurantLogoImg/台中-千田屋.jpeg', 'https://reurl.cc/9O3K6j', 'https://reurl.cc/VjMdY5'),
+(15, '台式', '叁食初', '臺中市北區西屯路一段492巷54弄4號', '均消$300', '0422085272', '11:00-01:00', '台中市', '/img/RestaurantLogoImg/台中-叁食初.jpeg', 'https://reurl.cc/2Djnvv', 'https://reurl.cc/Ddl085');
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `restaurantimglist`
+-- 資料表結構 `restaurantphotolist`
 --
 
-CREATE TABLE `restaurantimglist` (
+CREATE TABLE `restaurantphotolist` (
   `id` bigint(20) NOT NULL,
-  `restaurant_id` bigint(20) NOT NULL,
-  `restaurant_img` varchar(255) NOT NULL
+  `Restaurant_Id` varchar(255) NOT NULL,
+  `Restaurant_Photo` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `restaurantphotolist`
+--
+
+INSERT INTO `restaurantphotolist` (`id`, `Restaurant_Id`, `Restaurant_Photo`) VALUES
+(3, '11', '/img/Taichung/台中-咚豬咚豬/271243250_255529140023979_9106061032416141015_n.jpeg'),
+(4, '11', '/img/Taichung/台中-咚豬咚豬/271556278_255528946690665_28174361469228778_n.jpeg'),
+(5, '11', '/img/Taichung/台中-咚豬咚豬/271593030_255529366690623_3035157339762853859_n.jpeg'),
+(6, '11', '/img/Taichung/台中-咚豬咚豬/271596050_255528873357339_2362975046235639858_n.jpeg'),
+(7, '11', '/img/Taichung/台中-咚豬咚豬/271724379_255529423357284_5202644642743413062_n.jpeg'),
+(8, '11', '/img/Taichung/台中-咚豬咚豬/271729794_255529190023974_7872967343448511328_n.jpeg'),
+(9, '11', '/img/Taichung/台中-咚豬咚豬/51689737022_808d230e10_c.jpeg'),
+(10, '11', '/img/Taichung/台中-咚豬咚豬/51690531361_eba4906602_c.jpeg'),
+(11, '11', '/img/Taichung/台中-咚豬咚豬/51690797648_19b18199d7_c.jpeg'),
+(12, '11', '/img/Taichung/台中-咚豬咚豬/51691209289_c31597af3c_c.jpeg'),
+(13, '11', '/img/Taichung/台中-咚豬咚豬/51691421200_354b453998_c.jpeg');
 
 --
 -- 已傾印資料表的索引
@@ -137,6 +162,12 @@ ALTER TABLE `restaurant`
   ADD PRIMARY KEY (`restaurant_id`);
 
 --
+-- 資料表索引 `restaurantphotolist`
+--
+ALTER TABLE `restaurantphotolist`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
@@ -144,7 +175,7 @@ ALTER TABLE `restaurant`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `comment`
 --
 ALTER TABLE `comment`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `memberinfo`
@@ -156,7 +187,13 @@ ALTER TABLE `memberinfo`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `restaurant`
 --
 ALTER TABLE `restaurant`
-  MODIFY `restaurant_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `restaurant_id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `restaurantphotolist`
+--
+ALTER TABLE `restaurantphotolist`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
